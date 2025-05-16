30fa847ee7075af6df3bd4c6a301726059a18857
goutham018 <66243642+goutham018@users.noreply.github.com>
2025-05-16 09:01:28 +0530

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -12,14 +12,14 @@ resource "aws_lb" "alb" {
 # Define the Target Group with Health Check
 resource "aws_lb_target_group" "alb_target_group" {
     name        = "example-target-group"
-    port        = 8080
+    port        = 8000
     protocol    = "HTTP"
     vpc_id      = var.vpc_id
     target_type = "instance"
 
     health_check {
-        path                = "/login"
-        port                = "8080"
+        path                = "/"
+        port                = "8000"
         protocol            = "HTTP"
         interval            = 30
         timeout             = 5
@@ -44,5 +44,5 @@ resource "aws_lb_listener" "alb_listener" {
 resource "aws_lb_target_group_attachment" "example" {
   target_group_arn = aws_lb_target_group.alb_target_group.arn
   target_id        = var.instance_id
-  port             = 8080
+  port             = 8000
 }
